b4d204445a2a9ad028f0d57bf62ba2dfda98d70a
shreya1584 <patelshreya1584@gmail.com>
2025-08-23 10:41:15 +0530

    Added sign-in and sign-up with Clerk

--- components/header.jsx
+++ components/header.jsx
@@ -33,14 +33,14 @@ const Header=()=>{
           <div className="flex items-center space-x-2 md:space-x-4">
             <SignedIn>
               <Link href={"/dashboard"}>
-              <Button>
+              <Button variant="outline">
                 <LayoutDashboard className="h-4 w-4"/>
                 <span className="hidden md:block">
                 Industry Insights
                 </span>
               </Button>
               </Link>
-            </SignedIn>
+            
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button className="flex items-center gap-2">
@@ -73,13 +73,24 @@ const Header=()=>{
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
+            </SignedIn>
             <SignedOut>
               <SignInButton>
-                <Button variant={"outline"}>Sign In</Button>
+                <Button variant="outline">Sign In</Button>
               </SignInButton>
             </SignedOut>
             <SignedIn>
-              <UserButton />
+              <UserButton 
+              appearance={{
+                 elements: {
+                  avatarBox: "w-10 h-10",
+                  userButtonPopoverCard: "shadow-xl",
+                  userPreviewMainIdentifier: "font-semibold",
+                },
+              }}
+              afterSignOutUrl="/"
+              />
+
             </SignedIn>
           </div>
 
