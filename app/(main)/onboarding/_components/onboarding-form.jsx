eea4da66599d2e74bac6be55204f46f712b918ef
shreya1584 <patelshreya1584@gmail.com>
2025-09-01 22:41:55 +0530

    Add database design, models, and onboarding page with API

---
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { onboardingSchema } from "@/app/lib/schema";
+const OnboardingForm = ({industries}) => {
+
+    const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm({
+    resolver: zodResolver(onboardingSchema),
+  });
+
+  return <div>OnboardingForm</div>;
+  
+};
+
+export default OnboardingForm;
--- components/header.jsx
+++ components/header.jsx
@@ -17,9 +17,11 @@ import {
   ChevronDown,
   StarsIcon,
 } from "lucide-react";
+import{ checkUser } from "@/lib/checkUser";
 
+const Header = async () => {
+  await checkUser();
 
-const Header=()=>{
     return (
         <header className="fixed top-0 w-full border-b bg-background/80 blackdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
           <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
